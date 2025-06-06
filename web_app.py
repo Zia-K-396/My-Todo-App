6fd99a6588ead6db5e1bd57699d4baab77777f6e
Zia <ziak396396@gmail.com>
2025-06-06 11:53:03 +0530

    initial commit

---
+++ web_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todos_in_file=functions.get_todos()
+
+    todo = st.session_state['new_todo']+"\n"
+    if todo not in todos_in_file:
+        todos.append(todo)
+        functions.write_todos(todos)
+    else:
+        print("In file")
+
+
+
+
+st.title("ToDo App")
+st.subheader("This is my ToDo app")
+st.write("This app helps increase your productivity")
+
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="ToDo App",label_visibility="hidden", placeholder="Type in a ToDo", on_change=add_todo, key='new_todo')
+print("Heloo")
