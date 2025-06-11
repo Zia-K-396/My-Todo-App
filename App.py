263456c76d56e3c8500db5ba0b5cdb8327ae6c99
Zia <ziak396396@gmail.com>
2025-06-11 15:06:38 +0530

    initial deployment stage

--- App.py
+++ App.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
+user = st.text_input("Please enter your username to access your todo list")
+
+if not user:
+    st.stop()
+
+
+todos = functions.get_todos(user)
 
 def add_todo():
-    todos_in_file=functions.get_todos()
+    todos_in_file=functions.get_todos(user)
 
     todo = st.session_state['new_todo']+"\n"
     if todo not in todos_in_file:
         todos.append(todo)
-        functions.write_todos(todos)
+        functions.write_todos(todos,user)
     else:
         st.info("Already in the list!", icon="🚨")
 
@@ -24,9 +30,8 @@ for index,todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
-        functions.write_todos(todos)
+        functions.write_todos(todos,user)
         del st.session_state[todo]
         st.rerun()
 
 st.text_input(label="ToDo App",label_visibility="hidden", placeholder="Type in a ToDo", on_change=add_todo, key='new_todo')
-print("Heloo")
--- functions.py
+++ functions.py
@@ -1,13 +1,18 @@
-FILEPATH="todos.txt"
 
-def get_todos(filepath=FILEPATH):
+def get_todos(user):
     """ Read a text file and return the list of to-do items"""
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+    filepath=f"{user}_todos.txt"
+    try:
+        with open(filepath, "r") as file_local:
+            todos_local = file_local.readlines()
+        return todos_local
+    except FileNotFoundError:
+        return []
 
 
-def write_todos(todos_local, filepath=FILEPATH):
+
+def write_todos(todos_local, user):
     """ Write the to_do items in the text file"""
+    filepath=f"{user}_todos.txt"
     with open(filepath,"w") as file_local:  # same as file=open() but better closes the file automatically even if the program stops abruptly
         file_local.writelines(todos_local)
