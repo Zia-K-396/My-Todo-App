e5bd5b8545b001d4a6f843ab3429f8398986ec5a
Zia <ziak396396@gmail.com>
2025-06-11 15:51:09 +0530

    username feature added

--- App.py
+++ App.py
@@ -1,37 +1,44 @@
 import streamlit as st
 import functions
 
-user = st.text_input("Please enter your username to access your todo list")
-
-if not user:
-    st.stop()
-
-
-todos = functions.get_todos(user)
-
-def add_todo():
-    todos_in_file=functions.get_todos(user)
-
-    todo = st.session_state['new_todo']+"\n"
-    if todo not in todos_in_file:
-        todos.append(todo)
-        functions.write_todos(todos,user)
-    else:
-        st.info("Already in the list!", icon="🚨")
-
-
-
-
-st.title("My ToDo App")
-
-
-
-for index,todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos,user)
-        del st.session_state[todo]
+if "user" not in st.session_state:
+    user = st.text_input("""ENTER YOUR USERNAME TO ACCESS YOUR TODO LIST\n
+    (The username entered first time will be used throughout.\n
+     Please try to include alphabets and numbers in your username)""")
+    if st.button("Continue") and user:
+        st.session_state.user = user
         st.rerun()
+    else:
+        st.stop()
 
-st.text_input(label="ToDo App",label_visibility="hidden", placeholder="Type in a ToDo", on_change=add_todo, key='new_todo')
+else:
+    user = st.session_state.user
+    todos = functions.get_todos(user)
+
+    def add_todo():
+        todos_in_file=functions.get_todos(user)
+
+        todo = st.session_state['new_todo']+"\n"
+        if todo not in todos_in_file:
+            todos.append(todo)
+            functions.write_todos(todos,user)
+        else:
+            st.info("Already in the list!", icon="🚨")
+        st.session_state["new_todo"] = ""
+
+
+
+
+    st.title("My ToDo App")
+
+
+
+    for index,todo in enumerate(todos):
+        checkbox = st.checkbox(todo, key=todo)
+        if checkbox:
+            todos.pop(index)
+            functions.write_todos(todos,user)
+            del st.session_state[todo]
+            st.rerun()
+
+    st.text_input(label="ToDo App",label_visibility="hidden", placeholder="Type in a ToDo", on_change=add_todo, key='new_todo')
--- pages/About.py
+++ pages/About.py
@@ -1,4 +1,5 @@
 import streamlit as st
-st.write("""This app was developed by Zia an NITC student.\n
+st.write("""This app is developed by Zia an NITC student.\n
+Python language is used for the development.\n
 This app is intended to improve your productivity.\n
 For any assistance contact ziak396396@gmail.com""")
